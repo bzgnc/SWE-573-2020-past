dd38c6c7c5dfec5462526074d12a9e48e1a77771
bzgnc <uci235@gmail.com>
2021-01-31 04:32:59 +0300

    accounts app

--- reddimental/urls.py
+++ reddimental/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from reddimental import views
+from django.conf.urls import url, include
+from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('about/', views.about, name="about"),
-    path('analyze_sentiment/', views.analyze_sentiment, name="analyze_sentiment")
+    path('analyze_sentiment/', views.analyze_sentiment, name="analyze_sentiment"),
+    path('accounts/', include('accounts.urls'))
 ]
