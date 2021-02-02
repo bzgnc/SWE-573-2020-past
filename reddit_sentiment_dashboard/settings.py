21a65ed8f93eb70c5dee50f389f3c85c502ae59b
bzgnc <uci235@gmail.com>
2021-02-02 10:13:41 +0300

    runtime değişti

--- reddit_sentiment_dashboard/settings.py
+++ reddit_sentiment_dashboard/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import django_heroku
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -125,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+django_heroku.settings(locals())
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
